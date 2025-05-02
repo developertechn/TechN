4310dc782aae93f1f7c0471f00d216824a915d55
NAVNEET <developer.techn@gmail.com>
2025-05-02 20:45:01 +0530

    Adding Template Folder and Add index.html

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 import datetime
 import re
 from flask import Flask
-
+from markupsafe import escape
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Hello, Flask!"
+@app.route('/<name>')
+def hello(name=None):
+    return render_template('index.html', person=name)
+
+
+debug = True
